4991e904819cf22e2d1d3fa217dd916958d3891f
Abhishek Mishra <amishra42@sapient.com>
2016-09-14 01:26:19 +0530

    Adding code snippet to close db connection before exiting the app.

--- reportInterface/mmmdash/server.js
+++ reportInterface/mmmdash/server.js
@@ -28,6 +28,27 @@
     // start app ===============================================
     app.listen(port);
     console.log('Magic happens on port ' + port);
+	
+	
+	process.stdin.resume();//so the program will not close instantly
+
+	function exitHandler(options, err) {
+		mongoose.disconnect();
+		console.log('clean');
+		if (options.cleanup) console.log('clean');
+		if (err) console.log(err.stack);
+		if (options.exit) process.exit();
+	}
+
+	//do something when app is closing
+	process.on('exit', exitHandler.bind(null,{cleanup:true}));
+
+	//catches ctrl+c event
+	process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+
+	//catches uncaught exceptions
+	process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
+	
     exports = module.exports = app;
 })({});
 
