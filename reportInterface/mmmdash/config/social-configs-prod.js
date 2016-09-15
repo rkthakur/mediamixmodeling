8058e38e1aea9118a696257e981c5db12ea94156
Rakesh Thakur <rthakur@sapient.com>
2016-09-15 22:50:38 +0530

    Added logic to load configuration based on execution env

--- reportInterface/mmmdash/config/social-configs-prod.js
+++ reportInterface/mmmdash/config/social-configs-prod.js
@@ -1,8 +1,8 @@
 ﻿module.exports = {
     facebook: {
-        "FACEBOOK_APP_ID": "1646035832393408",
-        "FACEBOOK_APP_SECRET": "fd3eca55d01dea99251447fbb7e2ba87",
-        "CallbackURL": "http://dev-romimate.com:8088/login/facebook/return",
+        "FACEBOOK_APP_ID": "1643209252676066",
+        "FACEBOOK_APP_SECRET": "6443c4ee56cd4745a48714cfab69527d",
+        "CallbackURL": "http://www.romimate.com/login/facebook/return",
         "ProfileFields": ['id', 'displayName', 'email']
     },
     twitter: {
